7389e41e1167607c048f6802e59880949ee2ba3b
omh02033 <omh02033@gmail.com>
2021-10-06 18:41:22 +0900

    feat: clean code

--- src/pages/outing.tsx
+++ src/pages/outing.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useRef, useEffect } from "react";
+import React, { useState, useEffect } from "react";
 import Select from 'react-select';
 import styled from "@emotion/styled";
 import Btn from "../components/Btn";
