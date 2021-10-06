3f69e6e98820c0cad70d049e68c5d75302a7120e
omh02033 <omh02033@gmail.com>
2021-10-06 19:54:07 +0900

    feat: main page comeback func

--- src/pages/main.tsx
+++ src/pages/main.tsx
@@ -1,6 +1,8 @@
 import React, {useEffect, useState} from "react";
 import styled from "@emotion/styled";
 import {getCookie} from '../components/cookie';
+import axios from "axios";
+import io from "../api/socket";
 
 import Btn from "../components/Btn";
 
@@ -30,7 +32,28 @@ const Main: React.FC = () => {
     useEffect(() => {
         const user = getCookie('token');
         if(user) setIsLogin(true);
+        axios.post('/api/getClassNum')
+        .then((data:any) => {
+            io.emit('class', data.data.classNum);
+        }).catch(err => {
+            if(err.response.data.msg) {
+                alert(err.response.data.msg);
+            } else {
+                console.log(err);
+            }
+        });
     }, []);
+
+    const comeback = () => {
+        axios.post('/api/comeback')
+        .then((data:any) => {
+            io.emit("comeback", data.data.socketData);
+        })
+        .catch(err => {
+            if(err.response.data.msg) alert(err.response.data.msg);
+            console.log(err);
+        });
+    }
 
     return (
         <Conatiner>
@@ -38,7 +61,7 @@ const Main: React.FC = () => {
                 isLogin ? (
                     <Box>
                         <FieldBox onClick={() => {window.location.href="/outing"}}><Btn msg="외출" borderColor="#3BCD94" /></FieldBox>
-                        <FieldBox><Btn msg="복귀" borderColor="#A5A5A5" id="comeback" /></FieldBox>
+                        <FieldBox onClick={comeback}><Btn msg="복귀" borderColor="#A5A5A5" id="comeback" /></FieldBox>
                         <FieldBox onClick={() => {window.location.href="/status/16"}}><Btn msg="현황보기" borderColor="#A5A5A5" /></FieldBox>
                     </Box>
                 ) : (
